64de905ee2f3943ecd507ed66da4dd2b950c299b
AnikaJily <148711035+AnikaJily@users.noreply.github.com>
2023-11-03 03:19:08 +0300

    Update Program2.cs

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System;
 
 namespace lesson
 {
@@ -11,43 +9,40 @@ namespace lesson
         public string album;
         public int year;
     }
+
     class Program
     {
         public static void Print(string song, string singer, string album, int year)
         { 
-            Console.WriteLine("Песня: " + song);
+            Console.WriteLine("Название: " + song);
             Console.WriteLine("Исполнитель: " + singer);
             Console.WriteLine("Альбом: " + album);
-            Console.WriteLine("Год: " + year);
+            Console.WriteLine("Год выпуска: " + year);
             Console.WriteLine("  ");
         }
-        public static string name(string name)
+
+        public static string GetName(string prompt)
         { 
-            name = Console.ReadLine();
-            return name;
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
         }
-        public static int num(int num)
+
+        public static int GetNumber(string prompt)
         {
-            num = Convert.ToInt32(Console.ReadLine());
-            return num;
+            Console.WriteLine(prompt);
+            return Convert.ToInt32(Console.ReadLine());
         }
+
         public static void Main(string[] args)
         {
             Song b1 = new Song();
-            Console.WriteLine("Напишите песню, исполнителя, альбом и год: ");
-            b1.song = name(b1.song);
-            b1.singer = name(b1.singer);
-            b1.album = name(b1.album);
-            b1.year = num(b1.year);
-            b2.song = name(b2.song);
-            b2.singer = name(b2.singer);
-            b2.album = name(b2.album);
-            b2.year = num(b2.year);
+            Console.WriteLine("Напишите название песни, исполнителя, альбом и год выпуска: ");
+            b1.song = GetName("Песня: ");
+            b1.singer = GetName("Исполнитель: ");
+            b1.album = GetName("Альбом: ");
+            b1.year = GetNumber("Год выпуска: ");
+
             Print(b1.song, b1.singer, b1.album, b1.year);
-
         }
-
     }
-
-
-};
+}
